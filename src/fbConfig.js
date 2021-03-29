f3a995a4aef70116173a089e70e2c590b1e2a9cc
Will Cohen <whysgai@gmail.com>
2021-03-29 16:55:59 -0400

    chore: Add firebase config file

---
+++ src/fbConfig.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDDYfwcl9nuD6NrGgToNatyDgcROdZb54w",
+    authDomain: "cs7580-a5-message-board.firebaseapp.com",
+    projectId: "cs7580-a5-message-board",
+    storageBucket: "cs7580-a5-message-board.appspot.com",
+    messagingSenderId: "575151180871",
+    appId: "1:575151180871:web:d7469bf4bfca3e2c927562"
+    };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
