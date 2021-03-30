efce98884bc9311d0f01c126fc0972e28454a8cd
Will Cohen <whysgai@gmail.com>
2021-03-30 10:40:18 -0400

    Add auto-login to app

--- src/view/App.js
+++ src/view/App.js
@@ -1,16 +1,24 @@
 import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import ThreadList from '../components/ThreadList';
 import '../styles/App.css';
+import {LOGIN_STATE} from "../redux/storeConstants";
+import { validateUser } from "../redux/actions/loginActions"
 
 function App() {
+    const dispatch = useDispatch();
+    const loginState = useSelector(state => state.loginReducer.loginState);
+    const user = useSelector(state => state.userReducer.user);
 
     useEffect(() => {
-
-    }, []);
+        if (loginState === LOGIN_STATE.LOGGED_OUT) {
+            dispatch(validateUser("originator", "1234"));
+        }
+    });
 
     return (
         <div className="App">
-            <p>hi</p>
+            <p>hi { user !== null && user !== undefined ? user.id : "" }</p>
             <ThreadList />
         </div>
     );
