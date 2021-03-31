6ae2a432b17bb5d70dc51d4c7ab4d2e5fe437498
Will Cohen <whysgai@gmail.com>
2021-03-31 18:24:21 -0400

    feat: Imeplement add reply in reducer

--- src/redux/reducers/threadReducer.js
+++ src/redux/reducers/threadReducer.js
@@ -1,4 +1,4 @@
-import { READ_THREADS } from "../actionConstants";
+import { READ_THREADS, ADD_REPLY } from "../actionConstants";
 
 const INITIAL_STATE = [];
 
@@ -6,6 +6,23 @@ export const threadReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case READ_THREADS:
             return [...action.payload.threads];
+        case ADD_REPLY:
+            const mergedReplies = [
+                ...state.filter(thread =>
+                    action.payload.threadId === thread.id
+                )[0].replies, 
+                action.payload.reply
+            ];
+            const newState = state.map(thread => 
+                thread.id === action.payload.threadId ? 
+                    {
+                        ...thread,
+                        replies : mergedReplies
+                    }
+                    :
+                    thread
+            );
+            return newState;  
         default:
             return state;
     };
