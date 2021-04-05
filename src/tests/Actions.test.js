0ea7a439ddef0c88bb106620aec60581d0cef561
Will Cohen <will.r.cohen@gmail.com>
2021-04-05 14:47:15 -0400

    tests: Add test shells for action tests

--- src/tests/Actions.test.js
+++ src/tests/Actions.test.js
@@ -1,1 +1,31 @@
 test.skip("Placeholder", () => {});
+
+//login actions
+// login success
+test.skip("Generate login success action", () => {});
+// login fail
+test.skip("Generate invalid login action", () => {});
+// login error
+test.skip("Generate login error action", () => {});
+// validate user
+test.skip("Generate login success action on valid user action", () => {});
+// logout
+test.skip("Generate logout success action", () => {});
+
+//view actions
+// showList
+test.skip("Generate show list of threads action", () => {});
+// showSingle
+test.skip("Generate show single full thread action", () => {});
+
+//thread actions
+// readAllThreads
+test.skip("Successfully reads all threads", () => {});
+// readThreadByID
+test.skip("Successfully reads single thread by ID", () => {});
+// readThreadByTag
+test.skip("Successfully reads threads with given tags", () => {});
+// createThread
+test.skip("Successfully adds thread to existing list", () => {});
+// createReply
+test.skip("Successfully adds reply to given thread", () => {});
