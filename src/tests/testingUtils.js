99a3de52a3e5b41f96a42ca589949592b2470bab
Will Cohen <will.r.cohen@gmail.com>
2021-04-06 16:17:00 -0400

    test: Add customer render function for DOM testing

---
+++ src/tests/testingUtils.js
@@ -0,0 +1,20 @@
+import {render} from "@testing-library/react";
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import {rootReducer} from "./store";
+
+const customRender = (ui, initialState = {}) => {
+    const mockStore = createStore(rootReducer, initialState);
+    const Wrapper = ({children}) => {
+        return  (
+            <Provider store={mockStore}>
+                {children}
+            </Provider>
+        )
+    }
+
+    return render(ui, {wrapper: Wrapper})
+}
+
+export * from "@testing-library/react";
+export {customRender as render}
