0740df502b47122b6a3c9f711508cdaa40500d7d
Will Cohen <will.r.cohen@gmail.com>
2021-04-05 22:04:41 -0400

    tests: Implement thread action tests

--- src/tests/Actions.test.js
+++ src/tests/Actions.test.js
@@ -1,13 +1,41 @@
+import { _ } from "lodash";
 import { loginFail, loginNetworkError, loginSuccess, logout, validateUser } from "../redux/actions/loginActions";
 import * as loginActions from "../redux/actions/loginActions";
 import { showList, showSingle } from "../redux/actions/viewActions";
 import * as viewActions from "../redux/actions/viewActions";
+import { readAllThreads, readThreadByID, readThreadByTag, createThread, createReply } from "../redux/actions/threadActions";
 import * as threadActions from "../redux/actions/threadActions";
 import { LOGIN_SUCCESS, INVALID_LOGIN, LOGOUT, LOGIN_NETWORK_ERROR, VIEW_LIST, VIEW_SINGLE, 
     READ_THREADS, ADD_REPLY } from "../redux/actionConstants";
 
 
-test.skip("Placeholder", () => {});
+const SAMPLE_THREADS = [
+    {
+        id: "001",
+        author: "Starsky",
+        body: "First sample thread body here words #tag #more-tag #heyoo",
+        replies: [],
+        tags: ["#tag", "#more-tag", "#heyoo"],
+        timestamp: 1617569322,
+        title: "First"
+    },
+    {
+        id: "002",
+        author: "Hutch",
+        body: "Second but with more words and no tags and maybe some replies I don't really know this is just filler text why didn't I get lorem ipsum?",
+        replies: [
+            {
+                id: "021",
+                author: "Starsky",
+                body: "Heeeeeey bruh!",
+                timestamp: 1617569453
+            }
+        ],
+        tags: [],
+        timestamp: 1617569393,
+        title: "Second"
+    }
+];
 
 beforeAll(() => {
     //console.log("++++++++ ACTION TESTS +++++++++++++++");
@@ -41,7 +69,6 @@ test("Generate login error action", () => {
 });
 // validate user
 test("Generate login success action on valid user action", () => {
-
     const user = {
         id: "001",
         username: "apollo"
@@ -63,7 +90,6 @@ test("Generate login success action on valid user action", () => {
             }
         }
     }));
-
 
     expect(validateUser("apollo", "1234")).toEqual(expectedAction);
 });
@@ -103,12 +129,128 @@ test("Generate show single full thread action", () => {
 
 //thread actions
 // readAllThreads
-test.skip("Successfully reads all threads", () => {});
+test("Successfully reads all threads", () => {
+    const testThreads = _.cloneDeep(SAMPLE_THREADS);
+
+    const expectedAction = {
+        type: READ_THREADS,
+        payload: {
+            threads: testThreads
+        }
+    }
+
+    jest.spyOn(threadActions, "readAllThreads").mockImplementation(() => ({
+        type: READ_THREADS,
+        payload: {
+            threads: _.cloneDeep(SAMPLE_THREADS)
+        }
+    }));
+
+    expect(readAllThreads(testThreads)).toEqual(expectedAction);
+});
 // readThreadByID
-test.skip("Successfully reads single thread by ID", () => {});
+test("Successfully reads single thread by ID", () => {
+    const id = "002";
+
+    const testThreads = [_.cloneDeep(SAMPLE_THREADS[1])];
+
+    const expectedAction = {
+        type: READ_THREADS,
+        payload: {
+            threads: testThreads
+        }
+    }
+
+    jest.spyOn(threadActions, "readThreadByID").mockImplementation(() => ({
+        type: READ_THREADS,
+        payload: {
+            threads: [_.cloneDeep(SAMPLE_THREADS[1])]
+        }
+    }));
+
+    expect(readThreadByID(id)).toEqual(expectedAction);
+});
 // readThreadByTag
-test.skip("Successfully reads threads with given tags", () => {});
+test("Successfully reads threads with given tags", () => {
+    const tags = ["#heyoo"];
+
+    const testThreads = [_.cloneDeep(SAMPLE_THREADS[0])];
+
+    const expectedAction = {
+        type: READ_THREADS,
+        payload: {
+            threads: testThreads
+        }
+    }
+
+    jest.spyOn(threadActions, "readThreadByTag").mockImplementation(() => ({
+        type: READ_THREADS,
+        payload: {
+            threads: [_.cloneDeep(SAMPLE_THREADS[0])]
+        }
+    }));
+
+    expect(readThreadByTag(tags)).toEqual(expectedAction);
+});
 // createThread
-test.skip("Successfully adds thread to existing list", () => {});
+test("Successfully adds thread to existing list", () => {
+    const newThread = {
+        id: "003",
+        author: "Columbo",
+        body: "New thread for create thread test",
+        replies: [],
+        tags: ["#newthread"],
+        timestamp: 1617567421,
+        title: "Create Thread Test"
+    };
+
+    const testThreads = _.cloneDeep(SAMPLE_THREADS);
+    testThreads.push(_.cloneDeep(newThread));
+
+    const expectedAction = {
+        type: READ_THREADS,
+        payload: {
+            threads: testThreads
+        }
+    }
+
+    let finalThreads = _.cloneDeep(SAMPLE_THREADS);
+    finalThreads.push(_.cloneDeep(newThread));
+
+    jest.spyOn(threadActions, "createThread").mockImplementation(() => ({
+        type: READ_THREADS,
+        payload: {
+            threads: _.cloneDeep(finalThreads)
+        }
+    }));
+
+    expect(createThread(newThread)).toEqual(expectedAction);
+});
 // createReply
-test.skip("Successfully adds reply to given thread", () => {});
+test("Successfully adds reply to given thread", () => {
+    const testThreadId = "001"
+
+    const newReply = {
+        id: "012",
+        author: "Poirot",
+        body: "New reply for create reply test",
+    };
+
+    const expectedAction = {
+        type: ADD_REPLY,
+        payload: {
+            threadId: testThreadId,
+            reply: newReply
+        }
+    }
+
+    jest.spyOn(threadActions, "createReply").mockImplementation(() => ({
+        type: ADD_REPLY,
+        payload: {
+            threadId: testThreadId,
+            reply: newReply
+        }
+    }));
+
+    expect(createReply(testThreadId, newReply)).toEqual(expectedAction);
+});
