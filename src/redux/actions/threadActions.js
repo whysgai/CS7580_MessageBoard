626e2cfbc77a384d8b8299b2bad34d695cf571dd
Will Cohen <will.r.cohen@gmail.com>
2021-04-08 21:21:48 -0400

    fix: Fix not showing new thread on creation

--- src/redux/actions/threadActions.js
+++ src/redux/actions/threadActions.js
@@ -20,7 +20,6 @@ export const addReply = (threadId, reply) => ({
 });
 
 export const readAllThreads = () => {
-    console.log("Reading threads from server");
     return dispatch => {
         database.collection("threads").get()
             .then(querySnapshot => {
@@ -31,19 +30,16 @@ export const readAllThreads = () => {
                         ...doc.data()
                     })
                 })
-                console.log("Threads retreved from server", threads);
                 threads.sort((threadA, threadB) => threadA.timestamp - threadB.timestamp);
                 dispatch(readThreads(threads));
             })
             .catch(error => {
-                console.log("Login error", error);
-                // dispatch(loginNetworkError());
+                console.log("Server error", error);
             });
     };
 };
 
 export const readThreadByID = (id) => {
-    console.log("Reading single thread from server");
     return dispatch => {
         database.collection("threads").where("id", "==", id).get()
             .then(querySnapshot => {
@@ -54,19 +50,16 @@ export const readThreadByID = (id) => {
                         ...doc.data()
                     })
                 })
-                console.log("Threads retreved from server", threads);
                 threads.sort((threadA, threadB) => threadA.timestamp - threadB.timestamp);
                 dispatch(readThreads(threads));
             })
             .catch(error => {
-                console.log("Login error", error);
-                // dispatch(loginNetworkError());
+                console.log("Server error", error);
             });
     };
 };
 
 export const readThreadByTag = (searchTags) => {
-    console.log("Reading thread tags from server", searchTags);
     return dispatch => {
         database.collection("threads").where("tags", "array-contains-any", searchTags).get()
             .then(querySnapshot => {
@@ -77,19 +70,16 @@ export const readThreadByTag = (searchTags) => {
                         ...doc.data()
                     });
                 });
-                console.log("Threads retreved from server", threads);
                 threads.sort((threadA, threadB) => threadA.timestamp - threadB.timestamp);
                 dispatch(readThreads(threads));
             })
             .catch(error => {
-                console.log("Login error", error);
-                // dispatch(loginNetworkError());
+                console.log("Server error", error);
             });
     };
 };
 
 export const createThread = (thread) => {
-    console.log("Posting thread to server");
     const threads = store.getState().threads;
     return dispatch => {
         database.collection("threads")
@@ -99,9 +89,8 @@ export const createThread = (thread) => {
                     ...thread,
                     id:  newThread.id
                 }])
-                console.log("Updated threads after add", newThreads);
-                threads.sort((threadA, threadB) => threadA.timestamp - threadB.timestamp);
-                dispatch(readThreads(threads));
+                newThreads.sort((threadA, threadB) => threadA.timestamp - threadB.timestamp);
+                dispatch(readThreads(newThreads));
             })
             .catch(error => console.log(error));
     };
@@ -109,7 +98,6 @@ export const createThread = (thread) => {
 
 export const createReply = (threadId, reply) => {
     return dispatch => {
-        console.log("Adding reply", reply);
         database.collection("threads").doc(threadId)
             .update({
                 replies : firebase.firestore.FieldValue.arrayUnion(reply)
